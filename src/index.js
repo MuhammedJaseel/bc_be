1596ba975d486bc460a2df8eb0accdaca20a7144
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-12-24 17:23:15 +0400

    OP

--- src/index.js
+++ src/index.js
@@ -22,7 +22,10 @@ await connectDB();
 await connectLocalServer();
 
 app.get("", async (req, res) => {
-  console.log(req.hostname.split("vercel.app"));
+  return res.json({ app: "rpc1", status: "Working", version: "1.0.4" });
+});
+
+app.post("", async (req, res) => {
   if (IS_LOCAL_SERVER && req.hostname.split("vercel.app").length === 2) {
     try {
       const targetUrl = `${LOCAL_SERVER}${req.url}`;
@@ -35,21 +38,22 @@ app.get("", async (req, res) => {
         validateStatus: () => true,
       };
       const response = await axios(axiosConfig);
-      console.log("Responded from local");
       return res.status(response.status).send(response.data);
     } catch (err) {
-      // res.status(500).send("Server Error");
+      connectLocalServer();
+      return res.status(500).send("Server Error");
     }
   }
-  return res.json({ app: "rpc1", status: "Working", version: "1.0.4" });
-});
-
-app.post("", async (req, res) => {
   return res.json(await bcRouter(req.body));
 });
 
 app.get("/mine", async (req, res) => {
   return res.json(await mineTransactins());
+});
+
+app.get("/localconnection", async (req, res) => {
+  connectLocalServer();
+  return res.json({ succes: true });
 });
 
 const PORT = process.env.PORT || 4501;
